3515747ddaa477a40197de81e52b765c96b5cb3c
kostyaalehin <koss595959@gmail.com>
2022-06-07 18:02:17 +0500

    CollectionView Configuration

---
+++ PhotoTapps/PhotoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoTapps
+//
+//  Created by Константин Алехин on 07.06.2022.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
